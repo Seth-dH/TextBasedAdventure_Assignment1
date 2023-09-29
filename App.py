0fd5f7dd8d7ad22db2b4187519e29344ba31830c
Seth deHaan <78933122+Seth-dH@users.noreply.github.com>
2023-09-28 23:39:11 -0400

    Milestone 2
    all required files are in the project.
    created a very simple game intro

---
+++ App.py
@@ -0,0 +1,5 @@
+'''This is the method that deals with player interaction, along with displaying messages/story to the player'''
+
+# used for displaying text to the player
+def displayMessage(textToDisplay):
+    print (textToDisplay)
---
+++ Game.py
@@ -0,0 +1,8 @@
+import App
+
+def GameIntro():
+    App.displayMessage("Welcome to this game")
+    App.displayMessage("Here is some context: ")
+    App.displayMessage("You are a member of a bandit team in a futuristic dystopia. You are running a heist on a gang called <Gang2 Name>. You have been hired by <Gang1 Boss Name>, A Gang boss to find a memory chip Hidden inside <Gang2>’s headquarters. This memory chip was stolen from <Gang1> and has the location of their weapons storehouse. ")
+    
+GameIntro()
